0717f1a32bb9d9301910cc92c84f0fd82eead4bb
user <yannlefranc330@gmail.com>
2023-09-22 21:57:23 +0200

    creating javaok

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+                int photo = 999;
+
+                // Code here can use x
+                System.out.println(photo);
+
+
+    }
+}
